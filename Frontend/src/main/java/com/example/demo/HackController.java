00402022980bbf7ae938d07d0a1b3c7b6aa37492
hydriod <bibi6548@gmail.com>
2022-11-07 14:24:48 +0900

    add name form. 名前欄の追加

--- Frontend/src/main/java/com/example/demo/HackController.java
+++ Frontend/src/main/java/com/example/demo/HackController.java
@@ -1,27 +1,28 @@
 package com.example.demo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+
 @Controller
 public class HackController {
-
     @RequestMapping(value = "/index")
     private String indexPage() {
         return "/index.html";
     }
 
-    @RequestMapping(value = "/formStudent")
+    @RequestMapping(value = "/formStudent.html")
     private String formStudent(Model model) {
 
         return "/formStudent.html";
     }
 
-    @RequestMapping(value = "/formTeacher")
+    @RequestMapping(value = "/formTeacher.html")
     private String formTeacher(Model model) {
 
         return "/formTeacher.html";
@@ -29,35 +30,32 @@ public class HackController {
 
     @RequestMapping(value = "/postStudent")
     private String postStudent(
-        boolean day1time1, boolean day1time2, boolean day1time3,
-        boolean day2time1, boolean day2time2, boolean day2time3,
-        boolean day3time1, boolean day3time2, boolean day3time3,
-        boolean day4time1, boolean day4time2, boolean day4time3,
-        boolean day5time1, boolean day5time2, boolean day5time3,
-        boolean day6time1, boolean day6time2, boolean day6time3,
-        boolean day7time1, boolean day7time2, boolean day7time3,
-        String subject, int grade, String likeTeacher, String dislikeTeacher,
-        Model model
-    ) {
-        
-
+            String name,
+            boolean day1time1, boolean day1time2, boolean day1time3,
+            boolean day2time1, boolean day2time2, boolean day2time3,
+            boolean day3time1, boolean day3time2, boolean day3time3,
+            boolean day4time1, boolean day4time2, boolean day4time3,
+            boolean day5time1, boolean day5time2, boolean day5time3,
+            boolean day6time1, boolean day6time2, boolean day6time3,
+            boolean day7time1, boolean day7time2, boolean day7time3,
+            String subject, int grade, String likeTeacher, String dislikeTeacher,
+            Model model) {
 
         return "/submit.html";
     }
 
     @RequestMapping(value = "postTeacher")
     private String postTeacher(
-        boolean day1time1, boolean day1time2, boolean day1time3,
-        boolean day2time1, boolean day2time2, boolean day2time3,
-        boolean day3time1, boolean day3time2, boolean day3time3,
-        boolean day4time1, boolean day4time2, boolean day4time3,
-        boolean day5time1, boolean day5time2, boolean day5time3,
-        boolean day6time1, boolean day6time2, boolean day6time3,
-        boolean day7time1, boolean day7time2, boolean day7time3,
-        String subject, String grade,
-        Model model
-    ) {
-        
+            String name,
+            boolean day1time1, boolean day1time2, boolean day1time3,
+            boolean day2time1, boolean day2time2, boolean day2time3,
+            boolean day3time1, boolean day3time2, boolean day3time3,
+            boolean day4time1, boolean day4time2, boolean day4time3,
+            boolean day5time1, boolean day5time2, boolean day5time3,
+            boolean day6time1, boolean day6time2, boolean day6time3,
+            boolean day7time1, boolean day7time2, boolean day7time3,
+            String subject, String grade,
+            Model model) {
 
         return "submit.html";
     }
@@ -66,5 +64,19 @@ public class HackController {
     private String schedulePage(Model model) {
 
         return "/schedule.html";
+    }
+
+    private ArrayList<String> str2query(String form) {
+        ArrayList<String> query = new ArrayList<>();
+        
+        if (form != null) {
+            String[] lines = form.split("\n");
+            for (String line : lines) {
+                line = line.trim();
+                query.add(line);
+            }
+        }
+
+        return query;
     }
 }
