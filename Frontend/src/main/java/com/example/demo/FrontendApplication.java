3e68eadb57a2ef7efa81c8e7b6ef0dd6f7407b20
Hijiri ITO <cmj14156@cs-t219.cs.nitech.ac.jp>
2022-11-14 11:58:48 +0900

    プロジェクトの再生成

--- Frontend/src/main/java/com/example/demo/FrontendApplication.java
+++ Frontend/src/main/java/com/example/demo/FrontendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HackApplication {
+public class FrontendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HackApplication.class, args);
+		SpringApplication.run(FrontendApplication.class, args);
 	}
 
 }
